5a89efd10760e78fd53e40e2ff53f260129e588a
Arthur Milchior <Arthur@Milchior.fr>
2020-03-29 16:10:47 +0200

    pep8

--- __init__.py
+++ __init__.py
@@ -3,13 +3,16 @@ from aqt.browser import ChangeModel
 
 oldAccept = ChangeModel.accept
 
+
 def modSchema(self, check):
     pass
+
 
 def accept(self):
     oldModSchema = _Collection.modSchema
     _Collection.modSchema = modSchema
     oldAccept(self)
     _Collection.modSchema = oldModSchema
+
 
 ChangeModel.accept = accept
--- change20.py
+++ change20.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-#Copyright: Arthur Milchior arthur@milchior.fr
-#License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-#Feel free to contribute to this code on https://github.com/Arthur-Milchior/anki-change-note-type
-#Anki add-on number: 719871418
+# Copyright: Arthur Milchior arthur@milchior.fr
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+# Feel free to contribute to this code on https://github.com/Arthur-Milchior/anki-change-note-type
+# Anki add-on number: 719871418
 
 """
 "Change card's type" does not require a new upload of the database.
@@ -12,47 +12,50 @@ Note however that the number of leech, the easyness, the date of next review, et
 
 Update 2018-01-10: The creation date of note is preserved as much as possible (the date is incremented of a few miliseconds). The creation date of card is not preserved.
 """
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+import anki.notes
 from anki.hooks import addHook
+from anki.models import ModelManager
+from anki.utils import timestampID
 from aqt import mw
 from aqt.utils import tooltip
-from anki.utils import timestampID
-import anki.notes
-from anki.models import ModelManager
-
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 
 def oldChange(self, m, nids, newModel, fmap, cmap):
-        """Change the model of the nodes in nids to newModel
+    """Change the model of the nodes in nids to newModel
 
-        currently, fmap and cmap are null only for tests.
+    currently, fmap and cmap are null only for tests.
 
-        keyword arguments
-        m -- the previous model of the notes
-        nids -- a list of id of notes whose model is m
-        newModel -- the model to which the cards must be converted
-        fmap -- the dictionnary sending to each fields'ord of the old model a field'ord of the new model or to None. 
-        cmap -- the dictionnary sending to each card type's ord of the old model a card type's ord of the new model
-        """
-        assert newModel['id'] == m['id'] or (fmap and cmap)
-        if fmap:
-            self._changeNotes(nids, newModel, fmap)
-        if cmap:
-            self._changeCards(nids, m, newModel, cmap)
-        self.col.genCards(nids)
+    keyword arguments
+    m -- the previous model of the notes
+    nids -- a list of id of notes whose model is m
+    newModel -- the model to which the cards must be converted
+    fmap -- the dictionnary sending to each fields'ord of the old model a field'ord of the new model or to None. 
+    cmap -- the dictionnary sending to each card type's ord of the old model a card type's ord of the new model
+    """
+    assert newModel['id'] == m['id'] or (fmap and cmap)
+    if fmap:
+        self._changeNotes(nids, newModel, fmap)
+    if cmap:
+        self._changeCards(nids, m, newModel, cmap)
+    self.col.genCards(nids)
 
-def newChange(self,m,nids,newModel,fmap,cmap):
-        """As change. As side effect, change nids of cards. Return new nids """
-        new_nids=map(copyCards,nids)
-        oldChange(self,m,new_nids,newModel,fmap,cmap)
-        self.col.remNotes(nids)
-        self.col._remNotes(nids)
-        return new_nids
 
-ModelManager.change =newChange
+def newChange(self, m, nids, newModel, fmap, cmap):
+    """As change. As side effect, change nids of cards. Return new nids """
+    new_nids = list(map(copyCards, nids))
+    oldChange(self, m, new_nids, newModel, fmap, cmap)
+    self.col.remNotes(nids)
+    self.col._remNotes(nids)
+    return new_nids
 
-###Taken from Copy.py
+
+ModelManager.change = newChange
+
+# Taken from Copy.py
+
+
 def timestampID(db, table, t=None):
     "Return a non-conflicting timestamp for table."
     # be careful not to create multiple objects without flushing them, or they
@@ -61,19 +64,20 @@ def timestampID(db, table, t=None):
     while db.scalar("select id from %s where id = ?" % table, t):
         t += 1
     return t
+
+
 def copyCards(nids):
     mw.checkpoint("Copy Notes")
     mw.progress.start()
-    
-    
+
     # Copy notes
     for nid in nids:
-        print "Found note: %s" % (nid)
+        print(("Found note: %s" % (nid)))
         note = mw.col.getNote(nid)
         model = note._model
-        
+
         # Create new note
-        note_copy = anki.notes.Note(mw.col,model=model)
+        note_copy = anki.notes.Note(mw.col, model=model)
         # Copy tags and fields (all model fields) from original note
         note_copy.tags = note.tags
         note_copy.fields = note.fields
@@ -82,25 +86,25 @@ def copyCards(nids):
         note_copy.flush()
         mw.col.addNote(note_copy)
         nid_copy = note_copy.id
-        
-        cards_copy= note_copy.cards()
-        cards= note.cards()
-        ord_to_card = {card.ord:card for card in cards}
-        ord_to_card_copy = {card.ord:card for card in cards_copy}
+
+        cards_copy = note_copy.cards()
+        cards = note.cards()
+        ord_to_card = {card.ord: card for card in cards}
+        ord_to_card_copy = {card.ord: card for card in cards_copy}
         for card in cards:
             ord = card.ord
             card_copy = ord_to_card_copy.get(ord)
             if card_copy:
-                card.id=card_copy.id
+                card.id = card_copy.id
                 card.nid = nid_copy
             else:
                 tooltip("We copy a card which should not exists.")
-                card.id=timestampID(mw.col.db,"cards")
-                card.nid=nid_copy
+                card.id = timestampID(mw.col.db, "cards")
+                card.nid = nid_copy
             card.flush()
 
     # Reset collection and main window
     mw.col.reset()
     mw.reset()
-        
+
     tooltip(_("""Cards copied."""))
